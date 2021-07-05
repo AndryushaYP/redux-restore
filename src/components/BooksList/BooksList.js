40d0e41ba8d1035d5a660e6e6ce422cd6dcfb113
Andrey Kloninger <veto.aion@gmail.com>
2021-07-05 13:32:28 +0500

    [add]: подключил Redux-store к BooksList

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./BooksList.css";
 import BookItem from "../BookItem/BookItem";
+import WithBookstoreService from "../hoc/WithBookstoreService";
+import { booksLoaded } from "../../actions/actions";
 
-const BooksList = ({ books }) => {
-  return (
-    <ul>
-      {books.map((item) => (
-        <BookItem key={item.id} book={item} />
-      ))}
-    </ul>
-  );
+class BooksList extends Component {
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+
+    this.props.booksLoaded(data);
+  }
+
+  render() {
+    const { books } = this.props;
+    console.log(this.props);
+    return (
+      <ul>
+        {books.map((book) => (
+          <BookItem key={book.id} book={book} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = ({ books }) => {
+  return { books };
 };
 
-export default BooksList;
+const mapDispatchToProps = {
+  booksLoaded,
+};
+
+export default WithBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BooksList));
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,19 +2,7 @@ import React from "react";
 import BooksList from "../BooksList/BooksList";
 
 const HomePage = () => {
-  const books = [
-    {
-      id: 1,
-      title: "You dont know JS!",
-      author: "J.Simmons",
-    },
-    {
-      id: 2,
-      title: "Process",
-      author: "F.Kafka",
-    },
-  ];
-  return <BooksList books={books} />;
+  return <BooksList />;
 };
 
 export default HomePage;
